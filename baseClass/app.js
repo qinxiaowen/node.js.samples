94f5583f2cc667903729ab440754e3da3bfd39d5
qinxiaowen <453000697@qq.com>
2019-03-19 16:58:49 +0800

    1

---
+++ baseClass/app.js
@@ -0,0 +1,22 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    toString() {
+        return '(name is ' + this.name + '.)';
+    }
+}
+class Teacher extends Person {
+    teach() {
+        return '(i am teacher ' + this.name + ')';
+    }
+}
+
+var person = new Person("zhangsan");
+console.log(person.toString());
+
+var teacher = new Teacher("wang");
+console.log(teacher.teach());
+
+// (name is zhangsan.)
+// (i am teacher wang)
